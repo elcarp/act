c7a436376dc35cc8850bb2dfe41575da6761738f
elcarp <lise.carpenter@gmail.com>
2025-02-02 09:52:38 +0700

    styling

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,20 +2,34 @@
 import React from 'react'
 import ColourfulText from '~components/ui/colourful-text'
 import { motion } from 'motion/react'
+import hero from '~public/images/hero.jpg'
 
-export function ColourfulTextDemo() {
+export default function Hero() {
   return (
-    <div className='h-screen w-full flex items-center justify-center relative overflow-hidden bg-black'>
-      <motion.img
-        src='https://assets.aceternity.com/linear-demo.webp'
-        className='h-full w-full object-cover absolute inset-0 [mask-image:radial-gradient(circle,transparent,black_80%)] pointer-events-none'
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 0.5 }}
-        transition={{ duration: 1 }}
-      />
-      <h1 className='text-2xl md:text-5xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans'>
-        The best <ColourfulText text='components' /> <br /> you will ever find
-      </h1>
-    </div>
+    <>
+      <div className='h-screen-1/2 w-full flex flex-wrap items-center justify-center relative overflow-hidden bg-teal-800'>
+        <div>
+          <motion.img
+            src={hero.src}
+            className='h-full w-full object-cover absolute inset-0 [mask-image:radial-gradient(circle,transparent,black_80%)] pointer-events-none'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 0.5 }}
+            transition={{ duration: 1 }}
+          />
+          <h1 className='max-w-5xl text-2xl md:text-5xl text-center text-white relative z-2'>
+            Empowering and <span className='font-extrabold'><ColourfulText text='Elevating' /></span> <br /> Counseling
+            Standards in Thailand
+          </h1>
+          <div className='mt-10 mx-auto flex w-96 justify-center'>
+            <button className='w-40 mx-2 px-8 py-2 rounded-md bg-transparent text-white font-bold transition duration-200 hover:bg-teal-500 border-2 border-teal-500'>
+              Learn More
+            </button>
+            <button className='w-40 mx-2 px-8 py-2 rounded-md bg-teal-500 text-white font-bold transition duration-200 hover:bg-transparent border-2 border-transparent hover:border-teal-500'>
+              Apply
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      height: {
+        'screen-1/3': '33vh',
+        'screen-1/2': '50vh',
+        'screen-2/3': '66vh',
+        'screen-3/4': '75vh',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
