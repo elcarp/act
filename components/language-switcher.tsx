e21b73298ce00c6bbe4809a116ea4f6c06a11c4b
elcarp <lise.carpenter@gmail.com>
2025-02-27 21:37:01 +0700

    fix the language switcher

--- components/language-switcher.tsx
+++ components/language-switcher.tsx
@@ -3,20 +3,40 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+const defaultLocale = 'en-US'
+const locales = ['en-US', 'th-TH'] as const
+
 export default function LanguageSwitcher() {
   const pathname = usePathname()
-  const currentLang = pathname.split('/')[1]
+
+  // Function to get the current language
+  const getCurrentLang = (path: string): (typeof locales)[number] => {
+    const segments = path.split('/')
+    return locales.includes(segments[1] as (typeof locales)[number])
+      ? (segments[1] as (typeof locales)[number])
+      : defaultLocale
+  }
+
+  // Function to get the path without the language prefix
+  const getPathWithoutLang = (path: string, currentLang: string) => {
+    return currentLang === defaultLocale
+      ? path
+      : path.replace(`/${currentLang}`, '') || '/'
+  }
+
+  const currentLang = getCurrentLang(pathname)
+  const pathWithoutLang = getPathWithoutLang(pathname, currentLang)
 
   return (
     <div>
       <Link
-        href={`/en-US${pathname.slice(6)}`}
-        className={currentLang === 'en-US' ? 'font-bold' : ''}>
+        href={pathWithoutLang}
+        className={currentLang === defaultLocale ? 'font-bold' : ''}>
         English
       </Link>
       {' | '}
       <Link
-        href={`/th-TH${pathname.slice(6)}`}
+        href={`/th-TH${pathWithoutLang}`}
         className={currentLang === 'th-TH' ? 'font-bold' : ''}>
         ไทย
       </Link>
