df7035f95e3edde98e394944b8f83c6e18ba89b5
elcarp <lise.carpenter@gmail.com>
2025-03-01 11:07:18 +0700

    allow JSX in FAQ

--- app/[lang]/about-the-act/faq.tsx
+++ app/[lang]/about-the-act/faq.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, JSX } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { IconChevronDown, IconChevronUp } from '@tabler/icons-react'
 import { cn } from '~lib/utils'
@@ -7,36 +7,61 @@ import { cn } from '~lib/utils'
 const FAQs = [
   {
     question: 'Why should I become a member of ACT?',
-    answer:
-      'Joining ACT is essential for counselors in Thailand who are committed to maintaining professional standards, ethical practice, and accountability in their work. By becoming a member, you ensure that you are part of a network dedicated to upholding the highest standards in counseling, gaining access to resources that enhance your practice and ensuring your clients that you are qualified.',
+    answer: (
+      <p>
+        Joining ACT is essential for counselors in Thailand who are committed to
+        maintaining professional standards, ethical practice, and accountability
+        in their work. By becoming a member, you ensure that you are part of a
+        network dedicated to upholding the highest standards in counseling,
+        gaining access to resources that enhance your practice and ensuring your
+        clients that you are qualified.
+      </p>
+    ),
   },
   {
     question: 'Can I become a counselor by taking a short online course?',
-    answer:
-      "While there are many short courses available internationally due to the growing interest in mental health, becoming a qualified counselor involves years of education, practice, proper supervision, and accountability. Counseling is not just about theoretical knowledge; it requires practical skills, ethical standards, and professional oversight to ensure client safety. Taking shortcuts can harm not only your professional reputation but, more importantly, your client's mental and physical wellbeing.",
+    answer: (
+      <p>
+        While there are many short courses available internationally due to the
+        growing interest in mental health, becoming a{' '}
+        <strong>qualified counselor</strong> involves years of education,
+        practice, proper supervision, and accountability.
+      </p>
+    ),
   },
   {
     question: 'Why do I need a counseling network?',
-    answer:
-      'Engaging with other professionals provides an invaluable opportunity to share knowledge, insights, and effective strategies from diverse perspectives, enabling you to improve your practice and grow as a counselor. This collaborative approach fosters continuous learning, enhances professional competency, encourages innovation in addressing challenges within the field, and opens doors to partnerships and referrals.',
+    answer: (
+      <ul className='list-disc pl-5'>
+        <li>Share knowledge and insights.</li>
+        <li>Continuous learning and professional growth.</li>
+        <li>Expand your network for partnerships and referrals.</li>
+      </ul>
+    ),
   },
   {
     question: 'How much does it cost to become a licensed counselor?',
-    answer:
-      'ACT keeps membership fees affordable thanks to the  of the LEAF Foundation, making it accessible for counselors in Thailand to pursue licensure without undue financial burden. Please note that any external costs to maintain the license, such as Supervision or CPD courses, are to be covered by the applicants themselves. Our annual prices for each membership tier are listed below:',
+    answer: (
+      <p>
+        ACT keeps membership fees affordable thanks to the support of the
+        <strong> LEAF Foundation</strong>. Our annual prices for each membership
+        tier are listed below:
+      </p>
+    ),
   },
   {
     question:
       "What's the difference between a psychologist, psychiatrist, and a counselor? Do they all need licenses?",
-    answer:
-      'Psychologists, psychiatrists, and counselors all work in the mental health field, but they have different roles, training, and ways of helping people.',
-  },
-  {
-    question: 'How can I track my order?',
-    answer:
-      'You can track your order by logging into your account and visiting the order history page. You will also receive a tracking number via email once your order has shipped.',
+    answer: (
+      <p>
+        Psychologists, psychiatrists, and counselors all work in the mental
+        health field, but they have different roles, training, and ways of
+        helping people.
+      </p>
+    ),
   },
 ]
+
 export default function FAQ() {
   const [open, setOpen] = useState<string | null>(null)
   return (
@@ -63,7 +88,7 @@ const FAQItem = ({
   open,
 }: {
   question: string
-  answer: string
+  answer: string | JSX.Element
   open: string | null
   setOpen: (open: string | null) => void
 }) => {
@@ -72,13 +97,7 @@ const FAQItem = ({
   return (
     <div
       className='mb-8 w-full cursor-pointer rounded-lg bg-white p-4 shadow-input dark:bg-neutral-900'
-      onClick={() => {
-        if (isOpen) {
-          setOpen(null)
-        } else {
-          setOpen(question)
-        }
-      }}>
+      onClick={() => setOpen(isOpen ? null : question)}>
       <div className='flex items-start'>
         <div className='relative mr-4 mt-1 h-6 w-6 flex-shrink-0'>
           <IconChevronUp
@@ -101,25 +120,37 @@ const FAQItem = ({
           <AnimatePresence mode='wait'>
             {isOpen && (
               <motion.div
-                initial={{ height: 0 }}
-                animate={{ height: 'auto' }}
-                exit={{ height: 0 }}
-                transition={{ duration: 0.2, ease: 'easeOut' }}
-                className='overflow-hidden text-neutral-500 dark:text-neutral-400'>
-                {answer.split('').map((line, index) => (
-                  <motion.span
-                    initial={{ opacity: 0, filter: 'blur(5px)' }}
-                    animate={{ opacity: 1, filter: 'blur(0px)' }}
-                    exit={{ opacity: 0, filter: 'blur(0px)' }}
-                    transition={{
-                      duration: 0.2,
-                      ease: 'easeOut',
-                      delay: index * 0.005,
-                    }}
-                    key={index}>
-                    {line}
-                  </motion.span>
-                ))}
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: 'auto', opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+                transition={{ duration: 0.3, ease: 'easeOut' }}
+                className='overflow-hidden text-neutral-500 dark:text-neutral-400 mt-2'>
+                {typeof answer === 'string' ? (
+                  <motion.p
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -10 }}
+                    transition={{ duration: 0.3, ease: 'easeOut' }}>
+                    {answer}
+                  </motion.p>
+                ) : (
+                  <motion.div
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -10 }}
+                    transition={{ duration: 0.3, ease: 'easeOut' }}>
+                    {React.Children.map(answer, (child, index) => (
+                      <motion.div
+                        key={index}
+                        initial={{ opacity: 0, y: 10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        transition={{ duration: 0.2, delay: index * 0.1 }}>
+                        {child}
+                      </motion.div>
+                    ))}
+                  </motion.div>
+                )}
               </motion.div>
             )}
           </AnimatePresence>
