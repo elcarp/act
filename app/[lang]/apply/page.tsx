282f00b7637c2db7aff221d5c0394ac43a5fd0eb
elcarp <lise.carpenter@gmail.com>
2025-03-04 13:40:03 +0700

    create new page for apply

---
+++ app/[lang]/apply/page.tsx
@@ -0,0 +1,27 @@
+
+export default function About() {
+  return (
+    <>
+      <section className='py-20 bg-slate-100'>
+        <div className='max-w-3xl mx-auto px-10'>
+          <h1 className='text-2xl md:text-5xl lg:text-6xl'>Apply</h1>
+          <h3 className='text-lg mt-8'>Purpose & Use</h3>
+          <p className='mt-3'>
+            An ACT counseling license is a credential granted by the Accredited
+            Counselors of Thailand (ACT) to qualified counselors in Thailand who
+            meet the criteria. The purpose of obtaining an ACT counseling
+            license is to ethically provide counseling services in accordance
+            with international standards. A licensed counselor refers to a
+            professional who has met the standard requirements regarding
+            education, training, and ethical standards in providing counseling
+            services. In other countries, this is sometimes called a registered
+            counselor—the terms registered and licensed are interchangeable. An
+            accredited counselor goes one step further: this is a highly
+            experienced counselor who has held a license for at least five years
+            and has made significant contributions to the mental health field.
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
