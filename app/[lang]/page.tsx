1ae297f79a3cc9dc77490338ecc9f97683284c63
elcarp <lise.carpenter@gmail.com>
2025-03-11 12:39:16 +0700

    integrate CMS

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,14 +3,32 @@ import Hero from '~components/hero'
 import Introduction from '~components/introduction'
 import LatestNews from '~components/latest-news'
 
+export default async function Home() {
+  const contentful = require('contentful')
 
-export default function Home() {
+  const client = contentful.createClient({
+    space: 'mtdj6x8wk5bh',
+    environment: 'master', // defaults to 'master' if not set
+    accessToken: 'iwxZVZvv04tCvaFtvahaUFwIJgdZ1bZlt6qlaxhx7Cs',
+  })
+
+  client
+    .getEntry('6dj8bjEslrpGFqW2oZHnYK')
+    .then((entry: any) => console.log(entry))
+    .catch(console.error)
+
+  const response = await client.getEntries({
+    content_type: 'blogPost',
+  })
+
+  console.log(response)
+
   return (
     <>
       <Hero />
       <Introduction />
       <Features />
-      <LatestNews/>
+      <LatestNews />
     </>
   )
 }
