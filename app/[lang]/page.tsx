6e74ce42f2b337015254eb2f674d34527f75f140
elcarp <lise.carpenter@gmail.com>
2025-03-14 12:23:42 +0700

    resolve type error

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,27 +4,26 @@ import Introduction from '~components/introduction'
 import LatestNews from '~components/latest-news'
 import * as contentful from 'contentful'
 
-type PageProps = {
+type Props = {
   params: {
-    lang?: string
-    [key: string]: string | undefined
+    lang: string
   }
-  searchParams?: {
-    [key: string]: string | string[] | undefined
-  }
+  searchParams?: { [key: string]: string | string[] | undefined }
 }
 
-export default async function Home({ params }: PageProps) {
+export default async function Home({ params }: Props) {
   const client = contentful.createClient({
     space: `${process.env.CONTENTFUL_SPACE_ID}`,
     environment: 'master',
     accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
   })
 
-  client
-    .getEntry('6dj8bjEslrpGFqW2oZHnYK')
-    .then((entry) => console.log(entry))
-    .catch(console.error)
+  // Using await instead of then/catch for consistency
+  try {
+    await client.getEntry('6dj8bjEslrpGFqW2oZHnYK')
+  } catch (error) {
+    console.error(error)
+  }
 
   const response = await client.getEntries({
     content_type: 'homepageContent',
