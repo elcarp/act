7cded5e2aabe6bfee0a408f19970575ad2a4f130
elcarp <lise.carpenter@gmail.com>
2025-03-18 10:18:36 +0700

    remove error

--- components/nav.tsx
+++ components/nav.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
 import logo from '~public/images/act-logo.png'
-import LanguageSwitcher from './language-switcher'
+// import LanguageSwitcher from './language-switcher'
 import { useRouter } from 'next/navigation'
 
 export default function Nav() {
