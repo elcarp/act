1e9b14b2fef3bbcd97a87cc983a49b50105e68c4
elcarp <lise.carpenter@gmail.com>
2025-03-25 09:21:58 +0700

    replace with data from API

--- app/[lang]/act-membership-levels/overview.tsx
+++ app/[lang]/act-membership-levels/overview.tsx
@@ -1,165 +1,123 @@
-export default function Overview() {
+import { MembershipLevelFields } from './page'
+
+type Props = {
+  membershipLevels: MembershipLevelFields[]
+}
+
+export default function Overview({ membershipLevels }: Props) {
+  console.log(membershipLevels)
   return (
     <div>
       <h3 className='text-lg mt-8 font-bold text-center'>Overview</h3>
       <div className='table rounded-xl bg-slate-200 mt-4 shadow-lg text-xs lg:text-base'>
         <div className='grid grid-cols-6 sticky top-0'>
           <div className='bg-gray-300 p-4 font-bold'>Membership Type</div>
-          <div className='bg-gray-300 p-4 font-bold'>Junior Member (JM)</div>
-          <div className='bg-gray-300 p-4 font-bold'>Associate Member (AM)</div>
-          <div className='bg-gray-300 p-4 font-bold'>
-            Registered Counselor (RC)
-          </div>
-          <div className='bg-gray-300 p-4 font-bold'>
-            Accredited Counselor (AC)
-          </div>
-          <div className='bg-gray-300 p-4 font-bold'>Fellow Member (FM)</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-gray-300 p-4 font-bold'>{item.title}</div>
+          })}
         </div>
         <div className='grid grid-cols-6'>
           <div className='bg-white p-4 font-semibold'>Education Level</div>
-          <div className='bg-white p-4'>
-            Bachelor&apos;s or equivalent in Counseling, Psychology, Social
-            Work, or a related field
-          </div>
-          <div className='bg-white p-4'>
-            Bachelor&apos;s or equivalent in any field
-          </div>
-          <div className='bg-white p-4'>
-            Master&apos;s or equivalent in Counseling, Psychology, Social Work,
-            or a related field
-          </div>
-          <div className='bg-white p-4'>
-            Master&apos;s or equivalent in Counseling, Psychology, Social Work,
-            or a related field
-          </div>
-          <div className='bg-white p-4'>
-            Master&apos;s or equivalent in Counseling, Psychology, Social Work,
-            or a related field
-          </div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.educationLevel}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Minimum years in the field
           </div>
-          <div className='bg-white p-4'>N/A</div>
-          <div className='bg-white p-4'>N/A</div>
-          <div className='bg-white p-4'>1</div>
-          <div className='bg-white p-4'>5</div>
-          <div className='bg-white p-4'>10+</div>
+          {membershipLevels.map((item) => {
+            return (
+              <div className='bg-white p-4'>{item.minimumYearsInTheField}</div>
+            )
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Minimum Training Hours (BA/MA coursework, internships, trainings,
             certifications, CPD)
             <em className='block mt-3'>Form B</em>
           </div>
-          <div className='bg-white p-4'>
-            No minimum hours, must be enrolled in a counseling-related study
-          </div>
-          <div className='bg-white p-4'>
-            No minimum hours, must complete “Foundations of Counseling” course
-            in the first year of membership
-          </div>
-          <div className='bg-white p-4'>300</div>
-          <div className='bg-white p-4'>400</div>
-          <div className='bg-white p-4'>
-            Vast experience in the field and previous AC status of at least 3
-            years. Must have at least one publication or notable contribution to
-            the field.
-          </div>
+          {membershipLevels.map((item) => {
+            return (
+              <div className='bg-white p-4'>{item.minimumTrainingHours}</div>
+            )
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Client Contact Hours (face to face, phone, or online counseling)
             <em className='block mt-3'>Form C</em>
           </div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>200 (from the past 3 years)</div>
-          <div className='bg-white p-4'>450 (from the past 3 years)</div>
-          <div className='bg-white p-4'>-</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.clientContactHours}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Non-Contact Hours (team reporting, readings, practicum, workshops)
             <em className='block mt-3'>Form D</em>
           </div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>100</div>
-          <div className='bg-white p-4'>225</div>
-          <div className='bg-white p-4'>-</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.nonContactHours}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>Interview</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>Required</div>
-          <div className='bg-white p-4'>Required</div>
-          <div className='bg-white p-4'>Nomination</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.interview}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Supervision <em className='block mt-3'>Form E</em>
           </div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>30</div>
-          <div className='bg-white p-4'>100</div>
-          <div className='bg-white p-4'>-</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.supervision}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Supervisor reference <em className='block mt-3'>Form F</em>
           </div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>Required</div>
-          <div className='bg-white p-4'>Required</div>
-          <div className='bg-white p-4'>Nomination by other ACT members.</div>
+          {membershipLevels.map((item) => {
+            return (
+              <div className='bg-white p-4'>{item.supervisorReference}</div>
+            )
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Case study <em className='block mt-3'>Form G</em>
           </div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>Required</div>
-          <div className='bg-white p-4'>Required</div>
-          <div className='bg-white p-4'>-</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.caseStudy}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Reflective essay <em className='block mt-3'>Form H</em>
           </div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>Required</div>
-          <div className='bg-white p-4'>Publication</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.reflectiveEssay}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Annual Supervision <em className='block mt-3'>Form I-1</em>
           </div>
-          <div className='bg-white p-4'>12</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>12</div>
-          <div className='bg-white p-4'>12</div>
-          <div className='bg-white p-4'>-</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.annualSupervision}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Annual CPD <em className='block mt-3'>Form I-2</em>
           </div>
-          <div className='bg-white p-4'>16</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>32</div>
-          <div className='bg-white p-4'>40</div>
-          <div className='bg-white p-4'>-</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.annualCpd}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>
             Annual Activity <em className='block mt-3'>Form I-3</em>
           </div>
-          <div className='bg-white p-4 col-span-5'>
-            Attend at least one ACT workshop or networking event
-          </div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.annualActivity}</div>
+          })}
 
           <div className='bg-white p-4 font-semibold'>Audit</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>-</div>
-          <div className='bg-white p-4'>Random</div>
-          <div className='bg-white p-4'>Random</div>
-          <div className='bg-white p-4'>-</div>
+          {membershipLevels.map((item) => {
+            return <div className='bg-white p-4'>{item.audit}</div>
+          })}
         </div>
       </div>
     </div>
--- app/[lang]/act-membership-levels/page.tsx
+++ app/[lang]/act-membership-levels/page.tsx
@@ -1,12 +1,50 @@
-import AccreditedCounselor from './accredited-counselor'
-import AssociatedMember from './associated-member'
-import FellowMember from './fellow-member'
 import OrganizationalMember from './organizational-member'
 import Overview from './overview'
-import StudentMember from './student-member'
-import RegisteredCounselor from './registered-counselor'
+import * as contentful from 'contentful'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { Document } from '@contentful/rich-text-types'
+import { Entry } from 'contentful'
 
-export default function MembershipLevels() {
+export interface MembershipLevelFields extends contentful.EntrySkeletonType {
+  title: string
+  excerpt: string
+  fullDescription?: Document
+  annualFee: string
+  applicationFee?: string
+  educationLevel: string
+  minimumYearsInTheField: string
+  minimumTrainingHours: string
+  clientContactHours: string
+  nonContactHours: string
+  interview: string
+  supervision: string
+  supervisorReference: string
+  caseStudy?: string
+  reflectiveEssay: string
+  annualSupervision: string
+  annualCpd: string
+  annualActivity: string
+  audit: string
+  order: number
+}
+
+export default async function MembershipLevels() {
+  const client = contentful.createClient({
+    space: `${process.env.CONTENTFUL_SPACE_ID}`,
+    environment: 'master',
+    accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+  })
+
+  const membershipLevels = await client.getEntries<MembershipLevelFields>({
+    content_type: 'membershipLevel',
+    locale: 'en-US',
+  })
+  const sortedMembershipLevels = membershipLevels.items.sort((a, b) => {
+    return (Number(a.fields.order) ?? 0) - (Number(b.fields.order) ?? 0)
+  })
+
+  const finalMembershipLevels = sortedMembershipLevels.slice(0, -1)
+
   return (
     <>
       <section className='py-20 bg-slate-100'>
@@ -14,27 +52,159 @@ export default function MembershipLevels() {
           <h1 className='text-2xl md:text-5xl lg:text-6xl'>
             ACT Membership Levels
           </h1>
-          <div id='fellow-member' className='py-10'>
-            <FellowMember />
-          </div>
-          <div id='accredited-counselor' className='py-10'>
-            <AccreditedCounselor />
-          </div>
-          <div id='registered-counselor' className='py-10'>
-            <RegisteredCounselor />
-          </div>
-          <div id='associate-member' className='py-10'>
-            <AssociatedMember />
-          </div>
-          <div id='junior-member' className='py-10'>
-            <StudentMember />
-          </div>
+          {finalMembershipLevels &&
+            finalMembershipLevels.map((item: Entry<MembershipLevelFields>) => {
+              const description = item.fields.fullDescription
+              return (
+                <div key={item.sys.id}>
+                  <h3 className='text-lg mt-8 font-bold'>
+                    {String(item.fields.title || 'No Title')}
+                  </h3>
+                  <div className='mt-3'>
+                    {description &&
+                      documentToReactComponents(
+                        description as unknown as Document
+                      )}
+                    {item.fields.annualFee && (
+                      <span className='mt-3 block'>
+                        Annual fee: {String(item.fields.annualFee || 'N/A')}
+                      </span>
+                    )}
+                    {item.fields.applicationFee && (
+                      <span className='mt-3 block'>
+                        Application fee:{' '}
+                        {String(item.fields.applicationFee || 'N/A')}
+                      </span>
+                    )}
+                  </div>
+                  <div className='table rounded-xl bg-slate-200 p-5 mt-4 shadow-lg'>
+                    <div className='grid grid-cols-2 gap-4'>
+                      <div>
+                        <strong>{String(item.fields.title) || '-'}</strong>
+                      </div>
+                      <div>
+                        <strong>Requirements</strong>
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Education Level
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.educationLevel) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Minimum years in the field
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.minimumYearsInTheField) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Minimum Training Hours (BA/MA degree coursework,
+                        internships, trainings, workshops, certifications, CPD)
+                        <em className='block mt-3'>Form B</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.minimumTrainingHours) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Client Contact Hours (face to face, phone, or online
+                        counseling in an ongoing process)
+                        <em className='block mt-3'>Form C</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.clientContactHours) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Non-Contact Hours (intervision, team reporting,
+                        readings, practicum videos, team meetings, case
+                        referrals, emails, conducting workshops/trainings)
+                        <em className='block mt-3'>Form D</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.nonContactHours) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>Interview</div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.interview) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Supervision <em className='block mt-3'>Form E</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.supervision) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Supervisor reference{' '}
+                        <em className='block mt-3'>Form F</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.supervisorReference) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Case study <em className='block mt-3'>Form G</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.caseStudy) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Reflective essay <em className='block mt-3'>Form H</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.reflectiveEssay) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Annual Supervision{' '}
+                        <em className='block mt-3'>Form I-1</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.annualSupervision) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Annual Continual Professional Development (CPD){' '}
+                        <em className='block mt-3'>Form I-2</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.annualCpd) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>
+                        Annual Activity <em className='block mt-3'>Form I-3</em>
+                      </div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.annualActivity) || '-'}
+                      </div>
+
+                      <div className='bg-white p-4 rounded-xl'>Audit</div>
+                      <div className='bg-white p-4 rounded-xl'>
+                        {String(item.fields.audit) || '-'}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+
           <div id='organizational-member' className='py-10'>
             <OrganizationalMember />
           </div>
         </div>
         <div id='overview' className='container mx-auto p-3 md:p-10'>
-          <Overview />
+          <Overview
+            membershipLevels={finalMembershipLevels.map(
+              (item) => item.fields as unknown as MembershipLevelFields
+            )}
+          />
         </div>
       </section>
     </>
