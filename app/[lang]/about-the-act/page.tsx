bae11b66f01c7dec1b20bf0f2ff5c08d75c513ed
elcarp <lise.carpenter@gmail.com>
2025-03-27 14:40:20 +0700

    get about page data from API

--- app/[lang]/about-the-act/page.tsx
+++ app/[lang]/about-the-act/page.tsx
@@ -1,5 +1,8 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import FAQ from './faq'
 import * as contentful from 'contentful'
+import styles from './styles.module.css'
+import { Document } from '@contentful/rich-text-types'
 
 export interface FAQItem {
   fields: {
@@ -7,6 +10,13 @@ export interface FAQItem {
     question: string
     answer: Document
   }
+}
+
+interface AboutPageContent {
+  mainHeading: string
+  content: Document
+  partnershipsHeading: string
+  partnershipsContent: Document
 }
 
 export const revalidate = 60
@@ -24,6 +34,15 @@ export default async function About() {
     locale: 'en-US',
   })
 
+  const aboutPageContent = await client.getEntries<{
+    fields: AboutPageContent
+    contentTypeId: string
+  }>({
+    content_type: 'aboutPageContent',
+    locale: 'en-US',
+  })
+  console.log(aboutPageContent)
+
   const sortedFAQs = FAQs.items
     .map((item) => ({
       fields: {
@@ -36,40 +55,13 @@ export default async function About() {
 
   return (
     <>
-      <section className='py-20 bg-slate-100'>
-        <div className='max-w-3xl mx-auto px-10'>
+      <section className={`${styles.aboutPage} py-20 bg-slate-100`}>
+        <div className={`max-w-3xl mx-auto px-10`}>
           <h1 className='text-2xl md:text-5xl lg:text-6xl'>
-            What is an ACT Counseling Accreditation?
+            {aboutPageContent.items[0].fields.mainHeading}
           </h1>
-          <h3 className='text-lg mt-8'>Purpose & Use</h3>
-          <p className='mt-3'>
-            An ACT counseling accreditation is a credential granted by the
-            Accredited Counselors of Thailand (ACT) to qualified counselors in
-            Thailand who meet the criteria. The purpose of obtaining an ACT
-            counseling license is to ethically provide counseling services in
-            accordance with international standards. A registered counselor
-            refers to a professional who has met the standard requirements
-            regarding education, training, and ethical standards in providing
-            counseling services. An accredited counselor goes one step further:
-            this is a highly experienced counselor who has held a license for at
-            least five years and has made significant contributions to the
-            mental health field.
-          </p>
-          <h3 className='text-lg mt-10'>
-            Membership, License, and Accreditation
-          </h3>
-          <p className='mt-3'>
-            ACT membership is open to any individuals and organizations in the
-            counseling field, regardless of their accreditation status. ACT
-            currently offers five membership levels for individuals: three
-            designed to support both novice and experienced counselors as they
-            begin or continue their accreditation journey, one for retired
-            counselors who remain active in the field, and one for professionals
-            from other fields with an interest in counseling. Each level has
-            different requirements which reflect the counselor&apos;s experience
-            and excellence in their field. The description and requirements can
-            be found in the ACT Membership Levels tab
-          </p>
+          {aboutPageContent.items[0]?.fields?.content &&
+            documentToReactComponents(aboutPageContent.items[0].fields.content)}
         </div>
       </section>
       <section id='faq' className='py-20'>
@@ -82,8 +74,15 @@ export default async function About() {
       </section>
       <section className='py-20 bg-slate-100'>
         <div className='max-w-3xl mx-auto px-10'>
-          <h2 className='text-2xl md:text-5xl lg:text-6xl'>Partnerships</h2>
-          <p className='mt-10'>Coming soon.</p>
+          <h2 className='text-2xl md:text-5xl lg:text-6xl'>
+            {aboutPageContent.items[0].fields.partnershipsHeading}
+          </h2>
+          <div className='mt-10'>
+            {aboutPageContent.items[0]?.fields?.partnershipsContent &&
+              documentToReactComponents(
+                aboutPageContent.items[0].fields.partnershipsContent
+              )}
+          </div>
         </div>
       </section>
     </>
